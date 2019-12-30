68cacbda2bc52615e56b2c547248a9f39846256b
Tibor Nagy <tibornagy77@gmail.com>
2019-12-30 18:16:40 +0100

    IssueId #5
    Dependencies tested
    
    If appTest.js test fails, dependency and version check is required.

---
+++ test/appTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+describe('Check dependencies', function(){
+    it('Test should run without errors! If not, please check dependencies and versions!', function () {
+        let actual = 'hello';
+        let expected = 'hello';
+        assert.equal(actual, expected);
+    });
+});

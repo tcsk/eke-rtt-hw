242cdf7cbb4b8b532cf20f2c1ba795352192b544
Tibor Nagy <tibornagy77@gmail.com>
2019-12-30 21:41:39 +0100

    IssueId #8
    Getters implemented
    
    Getters are implemented and tested. Properties are accessibe only trough getters.

--- test/memoryTest.js
+++ test/memoryTest.js
@@ -22,4 +22,44 @@ describe('Memory', function () {
         };
         expect(() => new Memory(config)).to.throw('Invalid Memory configuration');
     });
+
+    it('getManufacturer should return correct value', function () {
+        let memory = new Memory(config);
+        assert.equal(memory.getManufacturer(), config.manufacturer);
+    });
+
+    it('manufacturer should be undefined', function () {
+        let memory = new Memory(config);
+        assert.isUndefined(memory.manufacturer);
+    });
+
+    it('getModel should return correct value', function () {
+        let memory = new Memory(config);
+        assert.equal(memory.getModel(), config.model);
+    });
+
+    it('model should be undefined', function () {
+        let memory = new Memory(config);
+        assert.isUndefined(memory.model);
+    });
+
+    it('getSize should return correct value', function () {
+        let memory = new Memory(config);
+        assert.equal(memory.getSize(), config.size);
+    });
+
+    it('size should be undefined', function () {
+        let memory = new Memory(config);
+        assert.isUndefined(memory.size);
+    });
+
+    it('getSpeed should return correct value', function () {
+        let memory = new Memory(config);
+        assert.equal(memory.getSpeed(), config.speed);
+    });
+
+    it('speed should be undefined', function () {
+        let memory = new Memory(config);
+        assert.isUndefined(memory.speed);
+    });
 });

a877ac857b2f0a3b40fe5fe140ab8b2e16ca6bd0
Tibor Nagy <tibornagy77@gmail.com>
2020-01-01 11:41:15 +0100

    IssueId #9
    MemoryList implemented
    
    MemoryList implemented and tested.

---
+++ test/memoryListTest.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const MemoryList = require('../src/collections/MemoryList');
+const ComputerPartList = require('../src/collections/ComputerPartList');
+const Memory = require('../src/domain/Memory');
+
+describe('MemoryList', function () {
+
+    it('MemoryList should be instantiated', function () {
+        let memoryList = new MemoryList();
+        assert.instanceOf(memoryList, MemoryList);
+    });
+
+    it('MemoryList should be an instance of ComputerPartList', function () {
+        let memoryList = new MemoryList();
+        assert.instanceOf(memoryList, ComputerPartList);
+    });
+
+    it('add should validate processor argument', function () {
+        let memoryList = new MemoryList();
+        let memory = {};
+        expect(() => memoryList.add(memory, 2500)).to.throw(
+            'memory is not a valid Memory');
+    });
+
+    it('add should validate processor price', function () {
+        let memoryList = new MemoryList();
+        let memory = new Memory({
+            'manufacturer': 'Kingston',
+            'model': 'Z693',
+            'size': 4096,
+            'speed': 2300
+        });
+        expect(() => memoryList.add(memory, 'abc')).to.throw(
+            'Price is not a valid number');
+    });
+});

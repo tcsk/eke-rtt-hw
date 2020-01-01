8e6a137c11c90fd1413054b2de2762a04c7f2445
Tibor Nagy <tibornagy77@gmail.com>
2020-01-01 16:21:48 +0100

    IssueId #12
    getOptimalConfigurationUnderPrices is implemented
    
    getOptimalConfigurationUnderPrices is tested and implemented.

--- src/util/ConfigurationFinder.js
+++ src/util/ConfigurationFinder.js
@@ -1,7 +1,26 @@
 class ConfigurationFinder {
 
-    constructor(motherboard, processor, memory) {
+    motherboardList;
+    processorList;
+    memoryList;
+
+    constructor(motherboardList, processorList, memoryList) {
         this.validate(...arguments);
+        this.motherboardList = motherboardList;
+        this.processorList = processorList;
+        this.memoryList = memoryList;
+    }
+
+    getOptimalConfigurationUnderPrices(motherboardPrice, processorPrice, memoryPrice) {
+        let motherboard = this.motherboardList.getOptimalUnderPrice(motherboardPrice);
+        let processor = this.processorList.getOptimalUnderPrice(processorPrice);
+        let memory = this.memoryList.getOptimalUnderPrice(memoryPrice);
+
+        return {
+            motherboard: motherboard,
+            processor: processor,
+            memory: memory
+        }
     }
 
     validate(...args) {
